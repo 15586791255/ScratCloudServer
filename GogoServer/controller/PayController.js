52fc1a06e00adbf0fb5d916b2709ca7f81bfc4db
huzhenjie <huzhenjie.dev@gmail.com>
2017-11-06 20:58:08 +0800

    测试支付宝回调

---
+++ GogoServer/controller/PayController.js
@@ -0,0 +1,9 @@
+
+const notifyALiPay = (req, res) => {
+    console.log(req);
+    res.send('success');
+};
+
+module.exports = {
+    notifyALiPay
+};
--- GogoServer/routes/Core.js
+++ GogoServer/routes/Core.js
@@ -8,6 +8,7 @@ const CommentController = require('../controller/CommentController');
 const TeamController = require('../controller/TeamController');
 const RaceController = require('../controller/RaceController');
 const CoinController = require('../controller/CoinController');
+const PayController = require('../controller/PayController');
 
 router.get('/test', function (req, res) {
     res.send({
@@ -25,5 +26,6 @@ router.get('/teams', TeamController.getTeams);
 router.get('/team/:team_id', TeamController.getTeam);
 router.get('/races', RaceController.getRaces);
 router.get('/coin/plans', CoinController.getPlans);
+router.post('/alipay/notify/:notify_id', PayController.notifyALiPay);
 
 module.exports = router;
