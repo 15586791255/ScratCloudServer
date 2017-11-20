117def62f694a2244101a8517e4e3dec5e4200e0
huzhenjie <huzhenjie.dev@gmail.com>
2017-11-21 01:02:55 +0800

    修改刷新token超时时间

--- AccountServer/dao/RefreshTokenDao.js
+++ AccountServer/dao/RefreshTokenDao.js
@@ -1,6 +1,6 @@
 function * addToken(uid, token, scope) {
     const createTs = new Date().getTime();
-    const expiredTs = createTs + Config.expiredTokenInTs;
+    const expiredTs = createTs + Config.expiredRefreshTokenInTs;
     const [insertId, affectedRows] = yield Conn.query('insert ignore into refresh_token set uid=?,token=?,scope=?,create_ts=?,expired_ts=?',
         {replacements: [uid, token, scope, createTs, expiredTs], type: Sequelize.QueryTypes.INSERT});
     return insertId;
